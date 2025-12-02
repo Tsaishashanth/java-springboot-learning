009e06387ae4769cd5b90d8b10a7f938792aed2f
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-12-02 17:18:34 +0530

    modified classes

---
+++ springboot/javaadvanced/src/Basics/Basics.java
@@ -0,0 +1,8 @@
+package Basics;
+//how java code gets executed
+//sourcecode -> javac -> bytecode
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("starting my basic java from here");
+    }
+}
--- springboot/javaadvanced/src/advanced/Main.java
+++ springboot/javaadvanced/src/advanced/Main.java
@@ -1,4 +1,4 @@
-package java.advanced;
+package advanced;
 
 public class Main {
     public static void main(String[] args) {
