38e71ca42c8f6daa87aa3d70653d7a790dec0a78
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-12-02 15:12:54 +0530

    modified intial commit to push main

--- springboot/javaadvanced/src/java/advanced/Main.java
+++ springboot/javaadvanced/src/java/advanced/Main.java
@@ -3,10 +3,7 @@ package java.advanced;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello starting my java and springboot journey here");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
